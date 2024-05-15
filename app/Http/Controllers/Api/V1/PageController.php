4d03a51a8935f57e65ab4a3ce37b933fecafcaf9
ramadan <ramadanabdo08@github.com>
2024-05-15 15:28:22 +0300

    add preferred setting

--- app/Http/Controllers/Api/V1/PageController.php
+++ app/Http/Controllers/Api/V1/PageController.php
@@ -13,7 +13,11 @@ use App\Models\City;
 use App\Models\Country;
 use App\Models\Slider;
 use App\Models\SubCategory;
+use App\Models\User;
+use App\Models\UserPreferedSetting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class PageController extends Controller
 {
@@ -71,6 +75,36 @@ class PageController extends Controller
 
 
         return apiResponse(true, $data, null, null, 200);
+    }
+
+    public function getPreferedSetting()
+    {
+        $data = UserPreferedSetting::where('user_id', Auth::id())->first();
+        return apiResponse(true, $data, null, null, 200);
+    }
+    public function changePreferedSetting(Request $request)
+    {
+        $validate = array(
+            'lang' => 'required|in,ar,en',
+            'currency_id' => 'required|exists:currencies,id',
+        );
+        $validatedData = Validator::make($request->all(), $validate);
+        if ($validatedData->fails()) {
+            return apiResponse(false, null, __('api.validation_error'), $validatedData->errors()->all(), 401);
+        }
+
+        $user = Auth::user();
+        if (!$user) {
+            return apiResponse(false, null, __('api.not_found'), null, 404);
+        }
+
+        $userPreferedSetting = UserPreferedSetting::updateOrCreate([
+            'user_id'   => $user->id,
+        ],[
+            'lang'   => $request->get('lang'),
+            'currency_id'    => $request->get('currency_id')
+        ]);
+        return apiResponse(true, $userPreferedSetting, __('api.update_success'), null, 200);
     }
 
 }
