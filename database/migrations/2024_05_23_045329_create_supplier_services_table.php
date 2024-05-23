5d629a9bcfb4304d112cd64d2066338ed13bfd09
Mustafa <mastafa3alie@gmail.com>
2024-05-23 03:06:01 +0200

    supplier

--- database/migrations/2024_05_23_045329_create_supplier_services_table.php
+++ database/migrations/2024_05_23_045329_create_supplier_services_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     public function up()
     {
 
-Schema::dropIfExists('supplier_services');
 
         Schema::create('supplier_services', function (Blueprint $table) {
             $table->id();
