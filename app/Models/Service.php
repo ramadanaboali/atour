fd07dd2c518c5229f3d400f32d008a993442ce18
Mustafa <mastafa3alie@gmail.com>
2024-07-20 00:25:58 +0200

    show services

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\App;
 
 class Service extends Model
 {
@@ -26,6 +27,29 @@ class Service extends Model
         'created_by',
         'updated_by'
     ];
+    protected $appends = ['photo','title','description'];
+    public function getPhotoAttribute()
+    {
+        return array_key_exists('image', $this->attributes) ? ($this->attributes['image'] != null ? asset('storage/services/' . $this->attributes['image']) : null) : null;
+
+    }
+
+     public function getTitleAttribute()
+    {
+        if(App::isLocale('en')) {
+            return $this->attributes['title_en'] ?? $this->attributes['title_ar'];
+        } else {
+            return $this->attributes['title_ar'] ?? $this->attributes['title_en'];
+        }
+    }
+    public function getDescriptionAttribute()
+    {
+        if(App::isLocale('en')) {
+            return $this->attributes['description_en'] ?? $this->attributes['description_ar'];
+        } else {
+            return $this->attributes['description_ar'] ?? $this->attributes['description_en'];
+        }
+    }
 
     public function city(): ?BelongsTo
     {
