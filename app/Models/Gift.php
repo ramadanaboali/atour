3dbbd158b0aa959bb233428552d4ba92f6195c9d
Mustafa <mastafa3alie@gmail.com>
2025-01-10 23:46:38 +0200

    trips

--- app/Models/Gift.php
+++ app/Models/Gift.php
@@ -20,7 +20,6 @@ class Gift extends Model
         'description_en',
         'description_ar',
         'price',
-        'location',
         'free_cancelation',
         'pay_later',
         'active',
