39a0a700b1764570a1970cf8b7975955a5c457cd
Mustafa <mastafa3alie@gmail.com>
2025-04-24 14:46:45 +0200

    pay later

--- app/Http/Requests/GiftRequest.php
+++ app/Http/Requests/GiftRequest.php
@@ -14,21 +14,53 @@ class GiftRequest extends FormRequest
 
     public function rules()
     {
-        return [
-            'title_en' => 'required|string|min:2',
-            'title_ar' => 'required|string|min:2',
-            'description_en' => 'required|string|min:2',
-            'description_ar' => 'required|string|min:2',
-            'price' => 'required|numeric',
-            'cover' => 'nullable|image',
-            'free_cancelation' => 'required|in:0,1',
-            'active' => 'required|in:0,1',
-            'pay_later' => 'required|in:0,1',
-            'city_id' => 'required|exists:cities,id',
-            'images' => 'required|array',
-            'images.*' => 'required|image',
-            'sub_category_ids' => 'required|array',
-            'sub_category_ids.*' => 'required|exists:sub_categories,id',
-        ];
+
+
+
+        switch ($this->method()) {
+            case 'GET':
+            case 'DELETE':
+                {
+                    return [];
+                }
+            case 'POST':
+                {
+                    return [
+                        'title_en' => 'required|string|min:2',
+                    'title_ar' => 'required|string|min:2',
+                    'description_en' => 'required|string|min:2',
+                    'description_ar' => 'required|string|min:2',
+                    'price' => 'required|numeric',
+                    'cover' => 'nullable|image',
+                    'free_cancelation' => 'required|in:0,1',
+                    'active' => 'required|in:0,1',
+                    'pay_later' => 'required|in:0,1',
+                    'city_id' => 'required|exists:cities,id',
+                    'images' => 'required|array',
+                    'images.*' => 'required|image',
+                    'sub_category_ids' => 'required|array',
+                    'sub_category_ids.*' => 'required|exists:sub_categories,id',
+                    ];
+                }
+            case 'PATCH':
+            case 'PUT':
+                {
+                    $rules = [
+                        'title_en' => 'required|string|min:2',
+                        'title_ar' => 'required|string|min:2',
+                        'description_en' => 'required|string|min:2',
+                        'description_ar' => 'required|string|min:2',
+                        'price' => 'required|numeric',
+                        'active' => 'required|in:0,1',
+                        'city_id' => 'required|exists:cities,id',
+                        'sub_category_ids' => 'required|array',
+                        'sub_category_ids.*' => 'required|exists:sub_categories,id',
+                    ];
+                    return $rules;
+                }
+            default:
+                return [];
+        }
+
     }
 }
