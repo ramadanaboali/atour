c97fe1e8eb9d6af3bfe46f173960da9a5f43faef
Mustafa <mastafa3alie@gmail.com>
2025-08-29 17:02:43 +0300

    suppliers

--- database/migrations/2025_08_29_002201_drop_columns_from_users_table.php
+++ database/migrations/2025_08_29_002201_drop_columns_from_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn(['temperory_email', 'temperory_phone','joining_date_from','joining_date_to','can_cancel','can_pay_later','ban_vendor','pay_on_deliver','bank_account','bank_name','bank_iban','tax_number','admin_value_type','admin_value']);
-            $table->unsignedBigInteger('country_id')->nullable()->after('city_id');
+            // $table->unsignedBigInteger('country_id')->nullable()->after('city_id');
         });
         Schema::table('suppliers', function (Blueprint $table) {
             $table->dropColumn(['tour_guid','profission_guide','experience_info','place_summary','place_content','expectations','languages','nationality','country_id','city_id']);
